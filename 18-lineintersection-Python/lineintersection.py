7836917bf2e7c6de5308403ded3d6ae3bf9b92ad
NeerajS943 <neerajchinnu9@msitprogram.net>
2021-08-25 15:32:49 +0530

    submission

--- 18-lineintersection-Python/lineintersection.py
+++ 18-lineintersection-Python/lineintersection.py
@@ -6,4 +6,18 @@
 
 def lineintersection(m1, b1, m2, b2):
 	# your code goes here
-	pass
+	if(b1==b2 or m1==m2):
+		return None
+	elif(isMultiple(m1,m2) or isMultiple(m2,m1)):
+		return None
+	else:
+		return (b2-b1)/(m1-m2)
+
+def isMultiple(a,b):
+	if a==0:
+		return True
+	elif b==0:
+		return False
+	elif a%b==0:
+		return True
+	return False
