e0d355e93f3f4a53db8e5a3d2492a931e06f6dbb
NeerajS943 <neerajchinnu9@msitprogram.net>
2021-08-25 16:19:08 +0530

    submission

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -3,8 +3,27 @@
 # can account as K-th the most frequent character then, print any one of them.
 
 
+import math
 
 def fun_kth_occurrences(s, n):
-	return 'a'
+	d = dict()
+	for i in s:
+		if i in d:
+			d[i] += 1
+		elif i.isspace():
+			pass
+		else:
+			d[i] = 1
+	
+	cnt = 1
+
+	while cnt<n:
+		max_num = max(d.values())
+		for k,v in d.items():
+			if max_num == v:
+				d[k] = 0
+				cnt += 1
+	
+	return max(d,key=d.get)
 
 
